aad8cf76f596889b248f301ab7c1c1c63698a0ef
Thaher Al Amir <thaher.alamir@hotmail.com>
2024-01-15 02:03:22 +0200

    First Commit for the portfolio

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const projects = [
+  {
+    title: "Movie Finder",
+    subtitle: "Javascript",
+    description:
+      "JS project made using themoviedb.org API to search for movies online.",
+    image: "MovieFinder.gif",
+    link: "https://github.com/thahertech/Movie-Finder-Database",
+  },
+  {
+    title: "Portfolio Project",
+    subtitle: "React, Javascript and Chakra UI",
+    description:
+      "Final Project for Meta: Front End Developer Course using a wide range of new tools!",
+    image: "PortfolioDEMO.gif",
+    link: "https://github.com/thahertech",
+  },
+  {
+    title: "Stil choosing Portfolio Worthy Demo",
+    subtitle: "React",
+    description:
+      "Meanwhile, why not checkout my other projects?",
+    link: "https://github.com/thahertech",
+  },
+  {
+    title: "Stil choosing Portfolio Worthy Demo",
+    subtitle: "React",
+    description:
+      "Meanwhile, why not checkout my other projects?",
+    link: "https://github.com/thahertech",
+  },
+];
+
+export const certificates = [
+  "Meta: Front-End Developer",
+  "Integrify: Full Stack Development"
+];
+
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "C++",
+  "Node",
+  "Linux",
+  "Tailwind CSS",
+];
